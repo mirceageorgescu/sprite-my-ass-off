3cc92f09864c22ae0dcae91348ff5ae6c292de00
Mircea Georgescu <mircea.georgescu@1and1.ro>
2012-07-30 13:50:35 +0300

    made the script to generate a less file with sprite coordinates

--- sprite.sh
+++ sprite.sh
@@ -1,42 +1,69 @@
 #!/bin/bash
-
-# spriteMyAssOff
 #
-# This script takes all the png's in a given folder, creates a sprite, 
-# optimizes it and displays the css styles needed to use the sprite.
-# It uses ImageMagick http://www.imagemagick.org and
-# OptiPNG http://optipng.sourceforge.net
+# Sprite My Ass Off
+#
+# This script takes all the png's in a given folder, creates a vertical sprite, 
+# optimizes it and generates a less file with the correct coordinates.
+#
+# In this case we have two folders: sprite-source-1x and sprite-source-2x.
+#
+# It uses
+#
+# ImageMagick http://www.imagemagick.org and
+# OptiPNG     http://optipng.sourceforge.net
 #
 # Copyright (c) 2012 Mircea Georgescu <mircea.georgescu@gmail.com>
 # MIT Licensed
 #
 
-function spriteMyAssOff {
+readonly LESS="sprite.less"
+
+function createSprite {
     # create the sprite
-    convert "${1}/*.png" -background transparent -append ${2}.png
-    echo "Converted ${1} files to ${2}"
-
-    # print css properties needed for each file    
-    echo "--------------------  CSS Styles for ${1}  -----------------------"
-    offset=0
-    for f in ${1}/*.png
-    do
-        width=`identify -format "%[fx:w]" $f`
-        height=`identify -format "%[fx:h]" $f`
-        filename=`identify -format "%f" $f`
-        echo "+ $filename"
-        echo "  width: ${width}px;"
-        echo "  height: ${width}px;"
-        echo "  background-position: 0 -${offset}px;"
-        echo "--------------------------------------"
-        let offset+=$height
-    done
-
+    convert "${1}/*.png" -background transparent -bordercolor none -border 0x${3} -append ${2}
+    echo "created ${2}"
     # optimize the sprite
-    optipng ${2}.png
+    optipng ${2}
 }
 
-# I call the function twice because I have two folders:
-# One for regular resolution and one for retina display
-spriteMyAssOff sprite-source@1x sprite@1x
-spriteMyAssOff sprite-source@2x sprite@2x
+# create normal @1x sprite
+createSprite sprite-source-1x sprite-1x.png 15
+# create retina @2x sprite
+createSprite sprite-source-2x sprite-2x.png 30
+
+# start writing the css file
+echo "// sprite css generated on `eval date +%d-%m-%Y-%H:%M`" > ${LESS}
+echo ".s, .s:after{" >> ${LESS}
+echo "  background-image: url(../img/sprite-1x.png);" >> ${LESS}
+echo "  background-repeat: no-repeat;" >> ${LESS}
+echo "}" >> ${LESS}
+
+# for each image in sprite-source-1x folder, add a class
+# in LESS file with the image coordonates
+let offset=15
+for f in sprite-source-1x/*.png
+do
+    width=`identify -format "%[fx:w]" $f`
+    height=`identify -format "%[fx:h]" $f`
+    filename=`identify -format "%f" $f`
+    #remove extension
+    name=${filename%.*}
+    #remove prefix_
+    className=${name##*_}
+    #replace '@' with ':'
+    className=${className//"@"/":"}
+    echo ".s.${className}{ background-position: 0 -${offset}px; }" >> ${LESS}
+    let offset+=$height+30
+done
+
+# get normal sprite width to set it as background-size for retina
+retinaBackgroundSize=`identify -format "%[fx:w]" sprite-1x.png`
+
+# in @2x media query overwrite background image path and add background size.
+# The coordinates remain the same as inherited from @1x sprite.
+echo "@media only screen and (-moz-min-device-pixel-ratio: 2), only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2) {" >> ${LESS}
+echo "  .s, .s:after{" >> ${LESS}
+echo "    background-image: url(../img/sprite-2x.png);" >> ${LESS}
+echo "    background-size: ${retinaBackgroundSize}px auto;" >> ${LESS}
+echo "  }" >> ${LESS}
+echo "}" >> ${LESS}
